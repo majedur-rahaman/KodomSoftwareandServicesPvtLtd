9b823b7b96320d8756175e3d87322cf3b5a74b07
shakil45 <sarifulkader@gmail.com>
2014-09-01 10:14:07 +0600

    Medicine Controller Done

---
+++ Controllers/MedicineController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_AutoSuggestion_E_AppointmentApp.Models;
+
+namespace Project_AutoSuggestion_E_AppointmentApp.Controllers
+{
+    public class MedicineController:Controller
+    {
+       private  AppContext db=new AppContext();
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(Medicine aMedicine)
+        {
+      
+            if (ModelState.IsValid)
+            {
+                if (aMedicine!=null)
+                {
+                    db.MedicinEntries.Add(aMedicine);
+                    db.SaveChanges();
+                    return RedirectToAction("Create");
+
+                }
+                
+            }
+            return RedirectToAction("Create");
+        }
+    }
+}
