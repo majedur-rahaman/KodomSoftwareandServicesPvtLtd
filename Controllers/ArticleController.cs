7b15d1dff83e6269d7b633e68b1e0e98bd21950e
harunrm <harungeo@yahoo.com>
2014-09-01 10:16:49 +0600

    Article Controller Done

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Project_AutoSuggestion_E_AppointmentApp.Models;
+
+namespace Project_AutoSuggestion_E_AppointmentApp.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private AppContext db = new AppContext();
+
+        // GET: /Article/
+        public ActionResult Index()
+        {
+            return View(db.Articles.ToList());
+        }
+
+        // GET: /Article/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Article article = db.Articles.Find(id);
+        //    if (article == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(article);
+        //}
+
+        // GET: /Article/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Article/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ArticleId,ArticleName,ArticleDescription,ArticaleImage")] Article article, HttpPostedFileBase image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    image.SaveAs(HttpContext.Server.MapPath("~/Images/") + image.FileName);
+                    article.ArticleImage = image.FileName;
+                }
+                db.Articles.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Create");
+            }
+            TempData["message"] = "Your article has been uploaded";
+            return View(article);
+        }
+
+        // GET: /Article/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Article article = db.Articles.Find(id);
+        //    if (article == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(article);
+        //}
+
+        // POST: /Article/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include="ArticleId,ArticleName,ArticleDescription,ArticaleImage")] Article article)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(article).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(article);
+        //}
+
+        // GET: /Article/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Article article = db.Articles.Find(id);
+        //    if (article == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(article);
+        //}
+
+        // POST: /Article/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Article article = db.Articles.Find(id);
+        //    db.Articles.Remove(article);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
